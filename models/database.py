fed470ab119680f75af7057fa455961bca7382bb
Syrena Chung <sy.uxdev@gmail.com>
2026-04-09 22:47:45 +0800

    Add db port

--- models/database.py
+++ models/database.py
@@ -12,6 +12,7 @@ db_config = {
     'user': os.getenv('DB_USER'),
     'password': os.getenv('DB_PASSWORD'),
     'host': os.getenv('DB_HOST'),
+    'port': int(os.getenv('DB_PORT', 3306)),
     'database': os.getenv('DB_DATABASE')
 }
 
